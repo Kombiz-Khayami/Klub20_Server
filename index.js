75c0ef3f1665ca428641fcef1a3f3d829e5b9c78
kombiz khayami <kombizkhayami@outlook.com>
2022-04-20 18:49:23 -0400

    added condole.log statment to home rout

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
   //if(req.headers.firstname) console.log("request has header " + req.headers['firstname'])
 
   if(req.url === '/'){
-    //console.log("hello");
+    console.log("hello");
      res.write("<h1>It's working 🤗</h1>");
      res.end();
   }
